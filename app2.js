d78b84a99c6a01aeb51cc445bf3678f2eb42f44d
DanyloHromyak <93078858+DanyloHromyak@users.noreply.github.com>
2022-04-30 14:47:26 +0300

    task 1,2 fully complete

---
+++ app2.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const username = prompt("Введіть ім'я");
+console.log(username);
+
+const age1 = 39;
+const age2 = 18;
+
+const ageSum = age1 + age2;
+
+alert(ageSum);
+
+const a = +prompt("First number");
+const b = +prompt("Second number");
+const sumOfNumbers = a * b;
+console.log(sumOfNumbers);
+
+let a1 = 5 + 3;
+let a2 = 5 - 3;
+let a3 = 5 * 3;
+const a4 = 5 / 3;
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+
+let a5 = 5 % 3;
+let a6 = 3 % 5;
+let a7 = 5 + "3";
+let a8 = "5" - 3;
+let a9 = 75 + "кг";
+
+alert(a5);
+alert(a6);
+alert(a7);
+alert(a8);
+alert(a9);
+
+const height = 23;
+let width = 10;
+
+const s = height * width;
+alert(s);
+
+const heightC = 10;
+const dC = 4;
+const v = Math.PI * Math.pow(dC / 2, 2) * heightC;
+console.log(v);
+
+const n = 3;
+const m = 4;
+const k = Math.sqrt(Math.pow(n, 2) + Math.pow(m, 2));
+alert(k);
